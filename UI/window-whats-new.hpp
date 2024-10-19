90215d6425f44e78d8e6589dd54d10495a3f2bf2
Matt Gajownik <matt@obsproject.com>
2024-10-19 14:51:34 +1100

    UI: Split out Whats New dialog, fix crash on shutdown
    CEF crashes on shutdown if any browser window remains open during the
    shutdown flow. Reproducible 100% of the time on Linux with What's New.
    The previous implementation of the What's New dialog correctly deleted
    all the Qt widgets, but the CEF internal event flow would only call the
    "preparing to close" function, and never the "window safely closed"
    function. Moving the code (practically unchanged) to a custom QDialog
    solves the problem entirely, and is more consistent with other uses.

---
+++ UI/window-whats-new.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QPointer>
+#include <QDialog>
+#include <string>
+
+class QCefWidget;
+
+class OBSWhatsNew : public QDialog {
+	Q_OBJECT
+
+	QCefWidget *cefWidget = nullptr;
+
+public:
+	OBSWhatsNew(QWidget *parent, const std::string &url);
+	~OBSWhatsNew();
+
+	virtual void reject() override;
+	virtual void accept() override;
+};
+
+namespace {
+QPointer<OBSWhatsNew> obsWhatsNew;
+}
